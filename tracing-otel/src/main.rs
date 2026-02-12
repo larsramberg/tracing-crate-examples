5a07d79de0692b29d648721496287eec79604825
larsramberg <larsivarramberg@yahoo.no>
2026-02-13 00:51:15 +0100

    feat: Add logging layer

--- tracing-otel/src/main.rs
+++ tracing-otel/src/main.rs
@@ -2,12 +2,17 @@ mod actors;
 
 use actors::Yak;
 use opentelemetry::{KeyValue, trace::TracerProvider as _};
-use opentelemetry_otlp::{Protocol, WithExportConfig};
-use opentelemetry_sdk::{Resource, metrics::PeriodicReader};
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::{LogExporter, Protocol, WithExportConfig};
+use opentelemetry_sdk::{
+    Resource,
+    logs::{SdkLogger, SdkLoggerProvider},
+    metrics::PeriodicReader,
+};
 use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
-use tracing_opentelemetry::{OpenTelemetryLayer, MetricsLayer};
 use std::{thread, time::Duration};
 use tracing::{info, instrument};
+use tracing_opentelemetry::{MetricsLayer, OpenTelemetryLayer};
 use tracing_subscriber::{Registry, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
@@ -39,19 +44,34 @@ fn shave_yaks(mut yaks: Vec<Yak>) {
 
 fn init_tracing() {
     let fmt_layer = fmt::layer();
-    
-    let exporter = opentelemetry_otlp::MetricExporter::builder().with_http().build().unwrap();
-    let reader = PeriodicReader::builder(exporter).with_interval(Duration::from_secs(1)).build();
-    let provider = opentelemetry_sdk::metrics::SdkMeterProvider::builder().with_reader(reader).build();
+
+    let exporter = opentelemetry_otlp::MetricExporter::builder()
+        .with_http()
+        .build()
+        .unwrap();
+    let reader = PeriodicReader::builder(exporter)
+        .with_interval(Duration::from_secs(1))
+        .build();
+    let provider = opentelemetry_sdk::metrics::SdkMeterProvider::builder()
+        .with_reader(reader)
+        .build();
     let metrics_layer = MetricsLayer::new(provider);
 
-    let subscriber = 
-        Registry::default()
+    let subscriber = Registry::default()
         .with(get_trace_layer())
         .with(metrics_layer)
+        .with(get_logging_layer())
         .with(fmt_layer);
 
     subscriber.init();
+}
+
+fn get_logging_layer() -> OpenTelemetryTracingBridge<SdkLoggerProvider, SdkLogger> {
+    let log_exporter = LogExporter::builder().with_http().build().unwrap();
+    let log_provider = SdkLoggerProvider::builder()
+        .with_batch_exporter(log_exporter)
+        .build();
+    OpenTelemetryTracingBridge::new(&log_provider)
 }
 
 fn get_trace_layer() -> OpenTelemetryLayer<Registry, opentelemetry_sdk::trace::Tracer> {
