12b05026dff6c33ec910c0f84a4923223d12635c
larsramberg <larsivarramberg@yahoo.no>
2026-02-12 23:19:40 +0100

    feat: Add metrics

--- tracing-otel/src/main.rs
+++ tracing-otel/src/main.rs
@@ -2,12 +2,12 @@ mod actors;
 
 use actors::Yak;
 use opentelemetry::{KeyValue, trace::TracerProvider as _};
-use opentelemetry_otlp::{ExportConfig, Protocol, WithExportConfig};
-use opentelemetry_sdk::Resource;
+use opentelemetry_otlp::{Protocol, WithExportConfig};
+use opentelemetry_sdk::{Resource, metrics::PeriodicReader};
 use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
-use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_opentelemetry::{OpenTelemetryLayer, MetricsLayer};
 use std::{thread, time::Duration};
-use tracing::instrument;
+use tracing::{info, instrument};
 use tracing_subscriber::{Registry, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
@@ -24,10 +24,13 @@ fn main() {
 
 #[instrument]
 fn shave_yaks(mut yaks: Vec<Yak>) {
+    let mut yaks_shaven = 0;
     for yak in &mut yaks {
         tracing::info!("I am shaving the yak...");
         if yak.shave() {
             tracing::info!("I managed to collect yak wool!");
+            yaks_shaven += 1;
+            info!(monotonic_counter.yaks_shaven = yaks_shaven);
         } else {
             tracing::warn!("Oh no, the yak is too mean to shave!");
         }
@@ -37,9 +40,15 @@ fn shave_yaks(mut yaks: Vec<Yak>) {
 fn init_tracing() {
     let fmt_layer = fmt::layer();
     
+    let exporter = opentelemetry_otlp::MetricExporter::builder().with_http().build().unwrap();
+    let reader = PeriodicReader::builder(exporter).with_interval(Duration::from_secs(1)).build();
+    let provider = opentelemetry_sdk::metrics::SdkMeterProvider::builder().with_reader(reader).build();
+    let metrics_layer = MetricsLayer::new(provider);
+
     let subscriber = 
         Registry::default()
         .with(get_trace_layer())
+        .with(metrics_layer)
         .with(fmt_layer);
 
     subscriber.init();
